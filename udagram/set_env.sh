5fce78247557f1b2adc91be770ad4ecf7bbe26cd
looksliketyler <91300303+looksliketyler@users.noreply.github.com>
2023-07-17 21:46:11 -0400

    init

--- udagram/set_env.sh
+++ udagram/set_env.sh
@@ -1,13 +1,13 @@
 # This file is used for convenience of local development.
 # DO NOT STORE YOUR CREDENTIALS INTO GIT
-export POSTGRES_USERNAME=enterpriseE
-export POSTGRES_PASSWORD=rega23341
-export POSTGRES_HOST=enterprisee.c4q8na1jsyg2.us-east-1.rds.amazonaws.com
+export POSTGRES_USERNAME=postgres
+export POSTGRES_PASSWORD=123123aa
+export POSTGRES_HOST=keywork-db.cayfliln7v9a.us-east-1.rds.amazonaws.com
 export POSTGRES_DB=postgres
-export AWS_BUCKET=arn:aws:s3:::keywork2113
+export AWS_BUCKET=arn:aws:s3:::keywork
 export AWS_DEFAULT_REGION=us-east-1
 export AWS_PROFILE=default
 export JWT_SECRET=mysecretstring
 export URL=http://localhost:8100
-export AWS_ACCESS_KEY_ID=AKIAZESYXY24N64YM6XB
-export AWS_SECRET_ACCESS_KEY=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
+export AWS_ACCESS_KEY_ID=AKIA4RLXLVF2NR4FLQWH
+export AWS_SECRET_ACCESS_KEY=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
