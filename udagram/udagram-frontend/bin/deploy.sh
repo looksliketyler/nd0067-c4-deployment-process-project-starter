4b02a44573ddc207f677ebb740d226d1ea7335e1
looksliketyler <91300303+looksliketyler@users.noreply.github.com>
2023-07-19 21:25:23 -0400

    update deploy

--- udagram/udagram-frontend/bin/deploy.sh
+++ udagram/udagram-frontend/bin/deploy.sh
@@ -1,2 +1,2 @@
-aws s3 cp --recursive --acl public-read ./www s3://keywork2113/
-aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://keywork2113/
+aws s3 cp --recursive --acl public-read ./www s3://keywork/
+aws s3 cp --acl public-read --cache-control="max-age=0, no-cache, no-store, must-revalidate" ./www/index.html s3://keywork/
